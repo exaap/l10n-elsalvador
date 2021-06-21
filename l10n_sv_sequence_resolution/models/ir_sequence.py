072c338525817abe0b2f4f48b21b096f631f85b5
Alejandro Olano <alejocode@gmail.com>
2021-06-21 13:12:30 -0500

    [ADD] Sequence Resolutions for El Salvador

---
+++ l10n_sv_sequence_resolution/models/ir_sequence.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Dominic Krimmer
+# Copyright 2016 Luis Alfredo da Silva (luis.adasilvaf@gmail.com)
+# Copyright 2019 Joan Marín <Github@JoanMarin>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import pytz
+from dateutil import tz
+from datetime import datetime
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class IrSequence(models.Model):
+    _inherit = 'ir.sequence'
+
+    use_dian_control = fields.Boolean(string='Use MH Resolutions Control?')
+    remaining_numbers = fields.Integer(string='Remaining Numbers',
+                                       default=False)
+    remaining_days = fields.Integer(string='Remaining Days', default=False)
